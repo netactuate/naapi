0249dedc05e1795f5846c4b87c950d43c823b76c
Kate Gerry <kgerry@outlook.com>
2022-01-20 11:48:57 -0800

    Updating the version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='naapi',
-    version='0.1.5',
+    version='0.1.6',
     packages=['naapi'],
     url='https://github.com/netactuate/naapi/',
     license='MIT',
